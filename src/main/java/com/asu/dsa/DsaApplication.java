9515e1c459677a32c43d70ee964f8c51a943985a
gudweiser <goleandr@gmail.com>
2021-02-21 00:47:55 +0100

    init

---
+++ src/main/java/com/asu/dsa/DsaApplication.java
@@ -0,0 +1,13 @@
+package com.asu.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asu/dsa/DsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asu.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

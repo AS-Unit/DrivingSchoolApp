5454666173d00bec545b6b5c027c6cf840114a24
gudweiser <goleandr@gmail.com>
2021-02-24 23:53:52 +0100

    f-005-vehicle-crud: add service

---
+++ src/main/java/com/asu/dsa/service/VehicleService.java
@@ -0,0 +1,57 @@
+package com.asu.dsa.service;
+
+import com.asu.dsa.model.Vehicle;
+import com.asu.dsa.repository.VehicleRepository;
+import com.asu.dsa.service.exception.NoVehicleFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    @Autowired
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    // get all vehicles
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    // get vehicle by id
+    public Vehicle getVehicleById(Long id) {
+        return vehicleRepository.findById(id)
+                .orElseThrow(()-> {
+                    throw new NoVehicleFoundException(id);
+                });
+    }
+
+    // update vehicle
+    public Vehicle updateVehicle(Long id) {
+        Vehicle vehicleToUpdate = vehicleRepository.findById(id)
+                .orElseThrow(()->{
+                    throw new NoVehicleFoundException(id);
+                });
+        return vehicleRepository.save(vehicleToUpdate);
+    }
+
+    // delete vehicle
+    public Vehicle deleteVehicle(Long id) {
+        Vehicle vehicleToDelete = vehicleRepository.findById(id)
+                .orElseThrow(()->{
+                    throw new NoVehicleFoundException(id);
+                });
+        vehicleRepository.delete(vehicleToDelete);
+        return vehicleToDelete;
+    }
+
+
+
+
+
+}

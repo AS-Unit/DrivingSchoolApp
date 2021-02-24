c271c7eb424f5bc9566d35417321954956318d0c
gudweiser <goleandr@gmail.com>
2021-02-24 23:28:30 +0100

    f-005-vehicle-crud: add exception

---
+++ src/main/java/com/asu/dsa/service/exception/NoVehicleFoundException.java
@@ -0,0 +1,7 @@
+package com.asu.dsa.service.exception;
+
+public class NoVehicleFoundException extends RuntimeException{
+    public NoVehicleFoundException (Long id) {
+        super(String.format("no vehicle with id:%s found!", id));
+    }
+}

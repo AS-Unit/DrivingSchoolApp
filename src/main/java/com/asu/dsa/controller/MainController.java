b1a3ca7b4c8a0ab8ab34a94cad50979b164aeaed
Sebastian Lewandowski <st.lewy@gmail.com>
2021-02-24 20:23:54 +0100

    f-014-main-controller: change controller

--- src/main/java/com/asu/dsa/controller/MainController.java
+++ src/main/java/com/asu/dsa/controller/MainController.java
@@ -1,2 +1,14 @@
-package com.asu.dsa.controller;public class MainController {
+package com.asu.dsa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/", "/index"})
+public class MainController {
+    @GetMapping
+    public String index() {
+        return "views/index";
+    }
 }
